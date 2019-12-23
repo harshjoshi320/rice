8e66a8012d1fae549e644d6af81e56ae4f4fd2e5
Harsh Joshi <harshjoshibpl@gmail.com>
2019-12-23 10:19:10 +0530

    Adjusted application autostart and window decorations, added display swithcer keybind

---
+++ i3/display_modes.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+CHOICE=$(echo 'Inbuilt  [eDP1]
+Switch To External  [HDMI1]
+Cast To External  = 
+Extend Display  + ' | rofi -config ~/.config/rofi/display_switch_config -dmenu -p "Select Display Mode")
+[[ -z $CHOICE ]] && exit
+
+case "$CHOICE" in
+	'Inbuilt  [eDP1]')
+		autorandr --load home
+		;;
+	'Switch To External  [HDMI1]')
+		autorandr --load external_only
+		;;
+	'Cast To External  = ')
+		autorandr --load screencast
+		;;
+	'Extend Display  + ')
+		autorandr --load presentation
+		;;
+	*)
+		autorandr --load home
+		;;
+esac
