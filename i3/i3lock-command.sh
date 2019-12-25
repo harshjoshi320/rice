3b4f699b2acd895c97c998d0cab56101475335cc
Harsh Joshi <harshjoshibpl@gmail.com>
2019-12-25 19:18:23 +0530

    Added lock and powermenu scripts

---
+++ i3/i3lock-command.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+i3lock -e \
+	-m \
+	--force-clock \
+	--insidevercolor=31454777 \
+	--insidewrongcolor=31454777 \
+	--insidecolor=31454777 \
+	--ringcolor=232627ff \
+	--ringvercolor=f7cc42ff \
+	--ringwrongcolor=cc0c23ff \
+	--line-uses-inside \
+	--keyhlcolor=00e59bff \
+	--bshlcolor=f91403ff \
+	--separatorcolor=232627ff \
+	--verifcolor=fff9eeff \
+	--wrongcolor=fff9eeff \
+	--indpos="960:810" \
+	--timestr="%I:%M %p" \
+	--timecolor=fff9eeff \
+	--timepos="960:540" \
+	--time-align "0" \
+	--time-font="Fira Sans" \
+	--timesize=36 \
+	--datestr="%A, %D" \
+	--datecolor=fff9eeff \
+	--datepos="960:580" \
+	--date-align="0" \
+	--date-font="Fira Sans" \
+	--datesize=24 \
+	--veriftext="   Verifying..." \
+	--wrongtext=" Wrong " \
+	--noinputtext="  " \
+	--radius=120 \
+	--ring-width=10 \
+	--image="Pictures/Japan/Shinjuku-Tokyo_sunset.jpg"
---
+++ rofi/powermenu.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+CHOICE=$(echo " Lock Session
+ Logout
+ Restart
+ Shutdown" | rofi  \
+	-config ~/.config/rofi/powermenu_config  \
+	-me-select-entry "" \
+	-me-accept-entry "MousePrimary" \
+	-dmenu -p "Choose Power Option")
+[[ -z $CHOICE ]] && exit
+
+function confirm_choice {
+	OP=$@
+	ACTION=$(echo "$OP
+Cancel" | rofi -dmenu -p "Confirm $OP")
+	[[ -z $ACTION ]] && exit 1
+
+	case "$ACTION" in
+		"$OP")
+			echo "go"
+			;;
+		"Cancel")
+			echo "x"
+			;;
+		*)
+			echo "x"
+			;;
+	esac
+}
+	
+case "$CHOICE" in
+	" Lock Session")
+		~/.config/i3/i3lock-command.sh
+		;;
+	" Logout")
+		if [ $(confirm_choice $CHOICE) = "go" ]; then
+			i3-msg exit
+		else
+			exit
+		fi
+		;;
+	" Restart")
+		if [ $(confirm_choice $CHOICE) = "go" ]; then
+			rofi -e "rebooting"
+		else
+			exit
+		fi
+		;;
+	" Shutdown")
+		if [ $(confirm_choice $CHOICE) = "go" ]; then
+			rofi -e "shutting down"
+		else
+			exit
+		fi
+		;;
+	*)
+		rofi -e "Huh?"
+		;;
+esac
