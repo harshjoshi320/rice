48aceb75a9a72a0e6f03283f05d7eb5a2ed77248
Harsh Joshi <harshjoshibpl@gmail.com>
2019-12-25 19:15:23 +0530

    Adjusted font color

--- polybar/scripts/player-mpris-simple.sh
+++ polybar/scripts/player-mpris-simple.sh
@@ -3,9 +3,9 @@
 player_status=$(playerctl status 2> /dev/null)
 
 if [ "$player_status" = "Playing" ]; then
-    echo "   $(playerctl metadata -f '%{T2}{{artist}}%{T-}') - $(playerctl metadata -f '%{T2}{{title}}%{T-}')"
+    echo "%{F#7FFF00}♪%{F-} $(playerctl metadata -f '%{T2}{{artist}}%{T-}') - $(playerctl metadata -f '%{T2}{{title}}%{T-}')"
 elif [ "$player_status" = "Paused" ]; then
-    echo "   $(playerctl metadata -f '%{T2}{{artist}}%{T-}') - $(playerctl metadata -f '%{T2}{{title}}%{T-}')"
+    echo "%{F#7FFF00}♪%{F-}%{F#777777} $(playerctl metadata -f '%{T2}{{artist}}%{T-}') - $(playerctl metadata -f '%{T2}{{title}}%{T-}')%{F-}"
 else
     echo " "
 fi
